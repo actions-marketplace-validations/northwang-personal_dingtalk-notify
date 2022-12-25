2038aeb3a9991882760c7264caf628fa1676ee6f
northwang <wyb_goodluck@163.com>
2022-12-25 21:34:19 +0800

    fix: Fix path for jar

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
 FROM openjdk:17
 
 COPY entrypoint.sh /entrypoint.sh
-COPY target/dingtalk-notify-1.0-jar-with-dependencies.jar /dingtalk-notify-1.0-jar-with-dependencies.jar
-
-RUN ls
-RUN pwd
+COPY target/dingtalk-notify-1.0-jar-with-dependencies.jar /dingtalk-notify.jar
 
 RUN chmod +x /entrypoint.sh
-RUN chmod +x /dingtalk-notify-1.0-jar-with-dependencies.jar
-
-RUN mv /dingtalk-notify-1.0-jar-with-dependencies.jar /dingtalk-notify.jar
+RUN chmod +x /dingtalk-notify.jar
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ if [ -z "$INPUT_PAYLOAD" ]; then
   exit 1
 fi
 
-if [ ! -e ./dingtalk-notify.jar ]; then
+if [ ! -e /dingtalk-notify.jar ]; then
   echo "Can not find file dingtalk-notify.jar in $(pwd)"
   exit 1
 fi
@@ -20,7 +20,7 @@ fi
 ENCODE_PAYLOAD=$(echo "$INPUT_PAYLOAD" | sed 's/{/%7B/g;s/}/%7D/g;s/:/%3A/g;s/\"/%22/g;s/,/%2C/g;s/ /%20/g')
 
 if [ -z "$INPUT_SECRET" ]; then
-  java -jar ./dingtalk-notify.jar -accessToken "$INPUT_ACCESS_TOKEN" -payload "$ENCODE_PAYLOAD"
+  java -jar /dingtalk-notify.jar -accessToken "$INPUT_ACCESS_TOKEN" -payload "$ENCODE_PAYLOAD"
 else
-  java -jar ./dingtalk-notify.jar -accessToken "$INPUT_ACCESS_TOKEN" -payload "$ENCODE_PAYLOAD" -secret "$INPUT_SECRET"
+  java -jar /dingtalk-notify.jar -accessToken "$INPUT_ACCESS_TOKEN" -payload "$ENCODE_PAYLOAD" -secret "$INPUT_SECRET"
 fi
